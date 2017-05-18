5cdac310787d0341183c106e6b6f07153efeb4d6
Haley Kalb <haley.kalb@gmail.com>
2017-05-17 18:34:05 -0600

    GET requests working

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -2,9 +2,44 @@
 'use strict';
 
 const express = require('express');
-
 const router = express.Router();
+const knex = require('../knex')
 
-// YOUR CODE HERE
+
+router.get('/', function(req, res) {
+  knex('classifieds')
+  .select(['id', 'title', "description", "price", "item_image"])
+    .then((classifieds) => {
+        res.json(classifieds);
+    })
+    .catch((err) => {
+        next(err);
+    });
+});
+
+router.get('/:id', function(req, res) {
+  knex('classifieds')
+  .select(['id', 'title', "description", "price", "item_image"])
+    .then((classifieds) => {
+        res.json(classifieds[0]);
+    })
+    .catch((err) => {
+        next(err);
+    });
+});
+
+// router.post('/', function(req, res) {
+//   knex('classifieds')
+//   .insert({
+//     title: req.body.title,
+//     description: req.body.desrciption,
+//     price: req.body.price,
+//   })
+//   .then(function(classifieds) {
+//     res.send(classifieds[0]);
+//   }).catch(function(err) {
+//     res.send(err);
+//   });
+// });
 
 module.exports = router;
